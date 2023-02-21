11ae3948d40fb9c8675b24f08624290aecffdbb1
Melissa Zuengler <melissazuengler@Melissas-MacBook-Pro.local>
2023-02-20 21:48:02 -0600

    added first prop

--- src/App.js
+++ src/App.js
@@ -14,15 +14,17 @@ class Square extends React.Component {
   render() {
     return (
       <button className="square">
-        {/* TODO */}
+        {this.props.value}
+
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i}/>;
   }
 
   render() {
